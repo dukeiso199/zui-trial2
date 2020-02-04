29ecd8e3a280cd32503022a307400f509e3387d4
islam dya <49913188+dukeiso199@users.noreply.github.com>
2020-02-04 13:05:07 +0200

    phase 2 : aimation adjust and phase 3: sounds added and loading bar

---
+++ Assets/methodsToCall.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class methodsToCall : MonoBehaviour
+{
+    public int sceneindex;
+    public Slider slider;
+    public Text progresstext;
+    public void Exitbutton()
+    {
+        Application.Quit();
+    }
+    public void loadlevel(int sceneindex)
+    {
+        StartCoroutine(AsynchronousLoad(sceneindex));
+    }
+    IEnumerator AsynchronousLoad(int sceneindex)
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync(sceneindex);
+
+
+        while (!ao.isDone)
+        {
+
+            float progress = Mathf.Clamp01(ao.progress / 0.9f);
+            slider.value = progress;
+            progresstext.text = progress * 100 + "%";
+
+            yield return null;
+        }
+    }
+}
